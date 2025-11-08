7335634a2d46a9e9e8a8f8011a8fceaafd447bcf
log <lrvanpel@asu.edu>
2025-11-07 17:12:43 -0800

    Support the commands

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,14 @@
 use std::env;
 use std::fs::{self, File};
 use std::io::Write;
+use std::iter::Peekable;
 use std::path::PathBuf;
 
-use anyhow::{Context, anyhow};
+use anyhow::{Context, Result, anyhow, bail, ensure};
 use chrono::{DateTime, Local};
 use directories::ProjectDirs;
 use serde::{Deserialize, Serialize};
 
-const VERBOSE: bool = true;
 const FILENAME: &str = "todofile.json";
 const TIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
 
@@ -39,46 +39,132 @@ struct TodoItem {
     completed: Option<DateTime<Local>>,
 }
 
+struct Cli {
+    verbose: bool,
+    path_filter: Option<PathBuf>,
+    command: Command,
+}
+
+enum Command {
+    AddItem { message: String },
+    DeleteItem { index: usize },
+    CompleteItem { index: usize },
+    ShowItems,
+}
+
+fn parse_message_command(args: &mut Peekable<env::Args>) -> Command {
+    Command::AddItem { message: args.into_iter().collect::<Vec<_>>().join(" ") }
+}
+
+fn parse_index(args: &mut Peekable<env::Args>) -> Result<usize> {
+    if let Some(arg) = args.next() {
+        arg.parse().map_err(|_| anyhow!("Could not parse \"{}\" as integer INDEX", arg))
+    } else {
+        bail!("Expected positional argument INDEX")
+    }
+}
+
+fn parse_delete_command(args: &mut Peekable<env::Args>) -> Result<Command> {
+    let index = parse_index(args)?;
+    ensure!(args.peek().is_none(), "Unknown arguments: {:?}", args.collect::<Vec<_>>());
+    Ok(Command::DeleteItem { index })
+}
+
+fn parse_complete_command(args: &mut Peekable<env::Args>) -> Result<Command> {
+    let index = parse_index(args)?;
+    ensure!(args.peek().is_none(), "Unknown arguments: {:?}", args.collect::<Vec<_>>());
+    Ok(Command::CompleteItem { index })
+}
+
+fn parse_show_command() -> Command {
+    Command::ShowItems
+}
+
+fn parse_args() -> Result<Cli> {
+    let mut args = env::args().peekable();
+    args.next().expect("Expected executable as first argument");
+
+    let mut verbose = false;
+
+    let command = loop {
+        match args.peek().map(String::as_str) {
+            Some("--verbose" | "-v") => {
+                args.next();
+                verbose = true;
+            }
+            Some("--delete" | "-d") => {
+                args.next();
+                break parse_delete_command(&mut args)?;
+            }
+            Some("--complete" | "-c") => {
+                args.next();
+                break parse_complete_command(&mut args)?;
+            }
+            Some(_) => break parse_message_command(&mut args),
+            None => break parse_show_command(),
+        }
+    };
+
+    Ok(Cli { verbose, path_filter: None, command })
+}
+
 fn main() -> anyhow::Result<()> {
+    let cli = parse_args()?;
+
     let project_dir = ProjectDirs::from("io.github", "logvp", "todoapp")
         .ok_or(anyhow!("Could not get project root"))?;
     let storage = project_dir.data_local_dir();
     let datafile = storage.join(FILENAME);
-    if VERBOSE {
+    if cli.verbose {
         println!("{}", storage.display());
         println!("{}", datafile.display());
     }
     fs::create_dir_all(storage)?;
     let text = fs::read_to_string(&datafile).ok();
-    let data: Vec<TodoItem> = if let Some(text) = text {
-        if VERBOSE {
+    let mut data: Vec<TodoItem> = if let Some(text) = text {
+        if cli.verbose {
             println!("{}", text);
         }
         serde_json::from_str(&text).context("While reading todo list from disk")?
     } else {
         Vec::new()
     };
-    let message = env::args().skip(1).collect::<Vec<String>>().join(" ");
-    if message.is_empty() {
-        for (i, item) in data.iter().enumerate() {
-            println!(
-                "{} - {} ({})",
-                i + 1,
-                item.message,
-                item.timestamp.format(TIME_FORMAT)
-            );
+
+    match cli.command {
+        Command::AddItem { message } => {
+            if cli.verbose {
+                println!("{}", message);
+            }
+            data.push(TodoItem {
+                timestamp: Local::now(),
+                path: env::current_dir()?,
+                message,
+                completed: None,
+            });
         }
-    } else {
-        println!("{}", message);
-        let mut data = data;
-        data.push(TodoItem {
-            timestamp: Local::now(),
-            path: env::current_dir()?,
-            message,
-            completed: None,
-        });
-        let mut f = File::options().create(true).open(&datafile)?;
-        writeln!(&mut f, "{}", serde_json::to_string(&data)?)?;
+        Command::DeleteItem { index } => {
+            data.remove(index.checked_sub(1).unwrap());
+        }
+        Command::CompleteItem { index } => {
+            data[index.checked_sub(1).unwrap()].completed = Some(Local::now());
+        }
+        Command::ShowItems => {
+            for (i, item) in data.iter().enumerate() {
+                println!(
+                    "{}{} - {} ({})",
+                    i + 1,
+                    if item.completed.is_some() { " ✓" } else { "" },
+                    item.message,
+                    item.timestamp.format(TIME_FORMAT)
+                );
+            }
+            // No need to update the list
+            return Ok(());
+        }
     }
+
+    // Finally, save the todo file
+    let mut f = File::options().create(true).write(true).truncate(true).open(&datafile)?;
+    writeln!(&mut f, "{}", serde_json::to_string(&data)?)?;
     Ok(())
 }
