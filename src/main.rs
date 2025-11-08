0224ea30384ab1771983aca41fa7bc3ef029a6d3
log <lrvanpel@asu.edu>
2025-11-07 21:15:24 -0800

    Use clap for arg parsing

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,12 @@
 
 use std::fs::{self, File};
 use std::io::Write;
-use std::iter::Peekable;
 use std::path::PathBuf;
 use std::{env, path};
 
-use anyhow::{Context, Result, anyhow, bail, ensure};
+use anyhow::{Context, anyhow};
 use chrono::{DateTime, Local};
+use clap::{ArgAction, Parser};
 use directories::ProjectDirs;
 use serde::{Deserialize, Serialize};
 
@@ -39,104 +39,26 @@ struct TodoItem {
     completed: Option<DateTime<Local>>,
 }
 
+#[derive(Parser, Debug)]
 struct Cli {
+    #[arg(short, long)]
     verbose: bool,
+
+    #[arg(short, long = "path", action = ArgAction::Append)]
     paths: Vec<PathBuf>,
-    command: Command,
-}
 
-enum Command {
-    AddItem { message: String },
-    DeleteItem { index: usize },
-    CompleteItem { index: usize },
-    ShowItems,
-}
+    #[arg(short, long, group = "command")]
+    delete: Option<usize>,
 
-fn parse_message_command(args: &mut Peekable<env::Args>) -> Command {
-    Command::AddItem {
-        message: args.into_iter().collect::<Vec<_>>().join(" "),
-    }
-}
+    #[arg(short, long, group = "command")]
+    complete: Option<usize>,
 
-fn parse_index(args: &mut Peekable<env::Args>) -> Result<usize> {
-    if let Some(arg) = args.next() {
-        arg.parse()
-            .map_err(|_| anyhow!("Could not parse \"{}\" as integer INDEX", arg))
-    } else {
-        bail!("Expected positional argument INDEX")
-    }
-}
-
-fn parse_delete_command(args: &mut Peekable<env::Args>) -> Result<Command> {
-    let index = parse_index(args)?;
-    ensure!(
-        args.peek().is_none(),
-        "Unknown arguments: {:?}",
-        args.collect::<Vec<_>>()
-    );
-    Ok(Command::DeleteItem { index })
-}
-
-fn parse_complete_command(args: &mut Peekable<env::Args>) -> Result<Command> {
-    let index = parse_index(args)?;
-    ensure!(
-        args.peek().is_none(),
-        "Unknown arguments: {:?}",
-        args.collect::<Vec<_>>()
-    );
-    Ok(Command::CompleteItem { index })
-}
-
-fn parse_show_command() -> Command {
-    Command::ShowItems
-}
-
-fn parse_args() -> Result<Cli> {
-    let mut args = env::args().peekable();
-    args.next().expect("Expected executable as first argument");
-
-    let mut verbose = false;
-    let mut paths = Vec::new();
-
-    let command = loop {
-        match args.peek().map(String::as_str) {
-            Some("--verbose" | "-v") => {
-                args.next();
-                verbose = true;
-            }
-            Some("--path" | "-p") => {
-                args.next();
-                if let Some(path) = args.next() {
-                    paths.push(fs::canonicalize(path)?);
-                } else {
-                    bail!("Expected argument following --filter");
-                }
-            }
-            Some("--delete" | "-d") => {
-                args.next();
-                break parse_delete_command(&mut args)?;
-            }
-            Some("--complete" | "-c") => {
-                args.next();
-                break parse_complete_command(&mut args)?;
-            }
-            Some(flag) if flag.starts_with("--") => {
-                bail!("Unknown argument \"{}\"", flag)
-            }
-            Some(_) => break parse_message_command(&mut args),
-            None => break parse_show_command(),
-        }
-    };
-
-    Ok(Cli {
-        verbose,
-        paths,
-        command,
-    })
+    #[arg(group = "command")]
+    message: Option<Vec<String>>,
 }
 
 fn main() -> anyhow::Result<()> {
-    let cli = parse_args()?;
+    let cli = Cli::parse();
 
     let project_dir = ProjectDirs::from("io.github", "logvp", "todoapp")
         .ok_or(anyhow!("Could not get project root"))?;
@@ -176,37 +98,33 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    match cli.command {
-        Command::AddItem { message } => {
-            if cli.verbose {
-                println!("{}", message);
-            }
-            data.push(TodoItem {
-                timestamp: Local::now(),
-                path: path::absolute(env::current_dir()?)?,
-                message,
-                completed: None,
-            });
+    if let Some(message) = cli.message {
+        let message = message.into_iter().collect::<Vec<_>>().join(" ");
+        if cli.verbose {
+            println!("{}", message);
         }
-        Command::DeleteItem { index } => {
-            data.remove(index.checked_sub(1).unwrap());
+        data.push(TodoItem {
+            timestamp: Local::now(),
+            path: path::absolute(env::current_dir()?)?,
+            message,
+            completed: None,
+        });
+    } else if let Some(index) = cli.delete {
+        data.remove(index.checked_sub(1).unwrap());
+    } else if let Some(index) = cli.complete {
+        data[index.checked_sub(1).unwrap()].completed = Some(Local::now());
+    } else {
+        for (i, item) in data.iter().enumerate() {
+            println!(
+                "{} [{}] {} ({})",
+                i + 1,
+                if item.completed.is_some() { "x" } else { " " },
+                item.message,
+                item.timestamp.format(TIME_FORMAT)
+            );
         }
-        Command::CompleteItem { index } => {
-            data[index.checked_sub(1).unwrap()].completed = Some(Local::now());
-        }
-        Command::ShowItems => {
-            for (i, item) in data.iter().enumerate() {
-                println!(
-                    "{} [{}] {} ({})",
-                    i + 1,
-                    if item.completed.is_some() { "x" } else { " " },
-                    item.message,
-                    item.timestamp.format(TIME_FORMAT)
-                );
-            }
-            // No need to update the list
-            return Ok(());
-        }
+        // No need to update the list
+        return Ok(());
     }
 
     // Finally, combine the selected and unselected items and save to disk
